d0c9b545e92b207d027ddf2cf243ece1c68faf88
Paschal Ndulue <paschal.nduleo@gmail.com>
2022-10-19 08:09:58 -0700

    added read me file, well.. not as detailed as i hoped

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+<!-- https://github.com/Haybuka/Yargs-Notesapp.git -->
+
 # Yargs-Notesapp
 Creating a command line notes app with yargs as a parser, and a json file as database.
+
+Modules
+- Chalk : Prettier,  but on terminal.
+- Yargs : Yargs helps you build interactive command line tools, by parsing arguments and generating an elegant user interface.
+- File system module (fs)
+
+What i learned
+   In this project, i was able to learn proper usage of the file system and how it connects with other modules (yargs, chalk) and their manipulation. Also, it brings to question the use of proper folder structure and syntax related to NodeJs (import as in the require usage, and export for locally made folders).
+
+Continued Development
+   This project can further be expanded using yargs, to query database, or pass and parse in values that require extra input as related to terminal.
+
+For more usage of the modules, do reference their documentations on npmjs.com
--- app.js
+++ app.js
@@ -1,53 +1,68 @@
 const fs = require('fs');
 const chalk = require('chalk')
-const {addNotes,listNotes,removeNotes, readNotes} = require('./notes/note')
 const yargs = require('yargs')
 
+const { addNotes, listNotes, removeNotes, readNotes } = require('./notes/note')
+
+//add command
 yargs.command({
-    command : 'add',
-    describe : 'add a note',
-    builder : {
-        title : {
-            describe : 'Note title',
-            demandOption:true,
-            type:'string'
+    command: 'add',
+    describe: 'add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
         },
-        body : {
-            describe : 'Note Body',
-            demandOption : true,
-            type : 'string'
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler : (argv) => {
-       addNotes(argv.title,argv.body)
+    handler: (argv) => {
+        addNotes(argv.title, argv.body)
     }
 
 })
+
+//remove command
 yargs.command({
-    command : 'remove',
-    description : 'remove a note',
-    handler : (argv) => {
+    command: 'remove',
+    description: 'remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
         removeNotes(argv.title)
     }
 })
+
+//list command
 yargs.command({
-    command : 'list',
-    description : 'list notes',
-    handler : () => {
+    command: 'list',
+    description: 'list notes',
+    handler: () => {
         listNotes()
     }
 })
+
+//read command
 yargs.command({
-    command : 'read',
-    description : 'read a note',
-    builder : {
-        title : {
-            demandOption : true,
-            describe : 'Note title',
-            type : 'string'
+    command: 'read',
+    description: 'read a note',
+    builder: {
+        title: {
+            demandOption: true,
+            describe: 'Note title',
+            type: 'string'
         }
     },
-    handler : (argv) => {readNotes(argv.title)}
+    handler: (argv) => { readNotes(argv.title) }
 })
 
 yargs.parse()
